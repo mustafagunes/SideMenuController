4f37d6176131c301caa198d2f402d00d09da613c
Mustafa GUNES <gunes149@gmail.com>
2018-12-07 16:52:29 +0300

    UI created

---
+++ MGSideMenu/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  MGSideMenu
+//
+//  Created by Mustafa GUNES on 7.12.2018.
+//  Copyright © 2018 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
